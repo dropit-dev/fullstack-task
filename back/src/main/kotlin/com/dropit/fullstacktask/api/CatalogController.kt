28e3ad16d56cc9323e6bf4dae4fc0cd0b4656e8f
Amir Attar <amira@dropitshopping.com>
2022-01-04 13:47:35 +0200

    cors handling

--- back/src/main/kotlin/com/dropit/fullstacktask/api/CatalogController.kt
+++ back/src/main/kotlin/com/dropit/fullstacktask/api/CatalogController.kt
@@ -2,11 +2,13 @@ package com.dropit.fullstacktask.api
 
 import com.dropit.fullstacktask.data.CatalogRepository
 import com.dropit.fullstacktask.models.Product
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
 @RequestMapping("/catalog")
+@CrossOrigin
 class CatalogController(val catalogRepository: CatalogRepository): CatalogApi {
 
     override fun getProducts(): List<Product> {
