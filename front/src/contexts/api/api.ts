becd10d8f3961b4fca4b554272f93513b9252d68
itaygolbary <itayg@dropitshopping.com>
2022-01-04 14:46:08 +0200

    fix bug

--- front/src/contexts/api/api.ts
+++ front/src/contexts/api/api.ts
@@ -3,29 +3,29 @@ import Axios from "axios";
 import { CatalogProduct, ProductDetails } from "../../modules/product/types";
 
 const AxiosClient = Axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "http://localhost:8080/catalog",
 });
 
 const API = {
   product: {
     getById: async (productId: number): Promise<ProductDetails> =>
-      AxiosClient.get<ProductDetails>(`/catalog/products/${productId}`).then(
+      AxiosClient.get<ProductDetails>(`/products/${productId}`).then(
         (response) => response.data
       ),
     getAll: async (): Promise<CatalogProduct[]> =>
-      AxiosClient.get<CatalogProduct[]>(`/catalog/products`).then(
+      AxiosClient.get<CatalogProduct[]>(`/products`).then(
         (response) => response.data
       ),
   },
   category: {
     getAll: async (): Promise<string[]> =>
-      AxiosClient.get<string[]>(`/catalog/categories`).then(
+      AxiosClient.get<string[]>(`/categories`).then(
         (response) => response.data
       ),
   },
   cart: {
-    postCheckout: async (cartData: Record<string, number>): Promise<void> =>
-      AxiosClient.post(`/checkout`, { cartData }),
+    postCheckout: async (cartData: Record<number, number>): Promise<void> =>
+      AxiosClient.post(`/checkout`, cartData),
   },
 };
 
